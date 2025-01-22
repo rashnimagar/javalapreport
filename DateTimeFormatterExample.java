5abf41fe4685a5526e703b1f0c5902d7a51687cf
rashnimagar <rashnithapamagar2@gmail.com>
2025-01-22 18:38:59 +0545

    Program to demonstrate Package

---
+++ DateTimeFormatterExample.java
@@ -0,0 +1,13 @@
+package myPackageFiles;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatterExample {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = now.format(formatter);
+        System.out.println("Formatted Date and Time: " + formattedDate);
+    }
+}
